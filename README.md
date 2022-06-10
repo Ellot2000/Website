65edbf85c8eb0b3162986050b5b48beeec714bea
Lukas <Lukas.eriksson@skola.taby.se>
2022-06-10 14:17:11 +0200

    alla sidor samma nav och footer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Website
+
+      <script id="replace_with_navbar" src="nav.js"></script>
+      <script src="hamburger.js"></script>
---
+++ hamburger.js
@@ -0,0 +1,17 @@
+console.log("hej");
+
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".nav-menu");
+let link = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+link.forEach((event) => event.addEventListener("click", closeHamburger));
+
+function closeHamburger() {
+  hamburger.classList.remove("active");
+  menu.classList.remove(".active");
+}
