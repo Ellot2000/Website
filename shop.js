dd3f48b17f2dda6b10e063426534f8fbaf338ad7
Lukas <Lukas.eriksson@skola.taby.se>
2022-06-23 12:38:57 +0200

    info card with event licener

--- shop.js
+++ shop.js
@@ -1,46 +1,42 @@
-console.log("Shop.js")
-let containerProducter = document.querySelector(".card-row")
-let addToCart = document.getElementsByClassName(".add-to-cart")
-
-
+console.log("Shop.js");
+let containerProducter = document.querySelector(".card-row");
+let addToCart = document.getElementsByClassName(".add-to-cart");
 
 let product = [
-    {
-        name: "Sko",
-        img: "background",
-        price: 6,
-        id: 1,
+  {
+    name: "Sko",
+    img: "background",
+    price: 6,
+    id: 1,
+  },
+  {
+    name: "Rolex",
+    img: "background",
+    price: 100,
+    id: 2,
+  },
+  {
+    name: "T-shirt",
+    img: "background",
+    price: 11,
+    id: 3,
+  },
+  {
+    name: "Keps",
+    img: "background",
+    price: 12,
+    id: 4,
+  },
+];
 
-    },
-    {
-        name: "Rolex",
-        img: "background",
-        price: 100,
-        id: 2,
+console.log(product);
 
-    },
-    {
-        name: "T-shirt",
-        img: "background",
-        price: 11,
-        id: 3,
-
-    },
-    {
-        name: "Keps",
-        img: "background",
-        price: 12,
-        id: 4,
-    },
-]
-
-
-function addObject(product){
-    product.forEach(function(cards){
-        const card = document.createElement("div");
-        card.classList.add("card-wrapper");
-        card.setAttribute("data-id", cards.id);
-        card.innerHTML = `
+function addObject(product) {
+  product.forEach(function (cards) {
+    const card = document.createElement("div");
+    card.classList.add("card-wrapper");
+    card.setAttribute("data-id", cards.id);
+    card.innerHTML = `
           
         <img src="img/${cards.img}.jpg" alt="" />
         <div class="info-wrapper">
@@ -48,28 +44,36 @@ function addObject(product){
             <p>${cards.name}</p>
             <p>${cards.price}$</p>
           </div>
-          <a href="#" class="add-to-cart">Add to cart</a>
+          
         </div>
           
       
-  `
+  `;
 
-    card.addEventListener("click", function(event){
-        console.log("hej")
-        const id = event.target.parentElement.getAttribute("data-id");
-        getApiMovieDetails(id);
-        
-    })
+    card.addEventListener("click", function (event) {
+      let id = event.target.parentElement.getAttribute("data-id");
+      getApiMovieDetails(cards.id);
+    });
 
     containerProducter.appendChild(card);
-    })
+  });
 }
 
+function getApiMovieDetails(id) {
+  console.log(id);
+  let newId = id - 1;
+  console.log(product[newId].name);
 
-function getApiMovieDetails(id){
-    console.log(product.id)
+  const infoCard = document.createElement("div");
+  infoCard.classList.add("info-card");
+  infoCard.innerHTML = `
+   <div>${product[newId].name}</div>
+  
+  
+  
+  
+  `;
+  document.body.appendChild(infoCard);
 }
 
-
-
-addObject(product)
+addObject(product);
